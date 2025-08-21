9d2c09c66efe18d8380cbbd3c6bd3e42e55cbf7c
Nalyanya Abraham <nalyanyaabraham1@gmail.com>
2025-08-21 17:34:09 +0300

    Implementation of the Review Serializer and the filter view

--- Movie_Review/Review/views.py
+++ Movie_Review/Review/views.py
@@ -3,6 +3,12 @@ from .models import Review, CustomUser
 from .forms import RegisterForm, ReviewForm
 from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Review
+from .serializers import ReviewSerializer
+from rest_framework.pagination import PageNumberPagination
 
 @login_required
 def submit_review(request):
@@ -42,4 +48,18 @@ def delete_review(request, pk):
 
     return render(request, 'delete_review.html', {'review': review})
 
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
 
+class ReviewView(APIView):
+    pagination_class = StandardResultsSetPagination
+
+
+    def get(self, request, movie_id):
+        reviews = Review.objects.filter(movie_id=movie_id)
+        serializer = ReviewSerializer(reviews, many=True)
+        paginator = self.pagination_class()
+        result_page = paginator.paginate_queryset(reviews, request)
+        return paginator.get_paginated_response(serializer.data)
