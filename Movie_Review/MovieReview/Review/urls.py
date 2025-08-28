fbed027d57854370a8e25580341e4082293be188
Nalyanya Abraham <nalyanyaabraham1@gmail.com>
2025-08-28 17:10:45 +0300

    replacing function based delete, update views with a generic views

--- Movie_Review/MovieReview/Review/urls.py
+++ Movie_Review/MovieReview/Review/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import ReviewView, RegistrationView,  SubmitView, update_review, delete_review, search_reviews
+from .views import RegistrationView,  SubmitView, UpdateReview, DeleteReview, ListReview
 
 urlpatterns = [
-    path('movies/<int:movie_id>/reviews/', ReviewView.as_view()),
     path('register/', RegistrationView.as_view(), name = 'register'),
     path('submit/', SubmitView.as_view, name='submit'), 
-    path('update0/<int:pk>/reviews/', update_review, name='update0'),
-    path('delete0/<int:pk>/reviews/', delete_review, name='delete0'),
-    path('search/', search_reviews, name='search'),
+    path('reviews/<int:pk>/update/', UpdateReview.as_view(), name='update'),
+    path('reviews/<int:pk>/delete/', DeleteReview.as_view(), name='delete'),
+    path('reviews/', ListReview.as_view(), name='reviews'),
     ]
--- Movie_Review/MovieReview/Review/views.py
+++ Movie_Review/MovieReview/Review/views.py
@@ -1,20 +1,13 @@
-from django.shortcuts import render, redirect 
+
 from .models import Review, CustomUser
-from .forms import ReviewForm, RegisterForm
-from django.shortcuts import get_object_or_404, redirect
-from django.contrib.auth.decorators import login_required
-from rest_framework.views import APIView
 from .serializers import ReviewSerializer, UserSerializer
 from rest_framework.pagination import PageNumberPagination
 from django.db.models import Q
-from django.contrib import messages
-from django.contrib.auth import login
 from rest_framework.response import Response
 from rest_framework import generics 
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
-from rest_framework.response import Response
 from django.db import IntegrityError
 from rest_framework.exceptions import PermissionDenied
 
@@ -30,16 +23,6 @@ class RegistrationView(generics.CreateAPIView):
             return Response({"detail": "Registration successful"})
         except IntegrityError:
             return Response({"error": "Username already exists"}, status=400)
-
-@login_required
-def submit_review(request):
-    if request.method == 'POST':
-        review_form = ReviewForm(request.POST)
-        if review_form.is_valid():
-            review = review_form.save(commit=False)
-            review.user = request.user
-            review.save()
-            return redirect('review_detail')
         
 class SubmitView(generics.CreateAPIView):
     queryset= Review.objects.all()
@@ -50,68 +33,24 @@ class SubmitView(generics.CreateAPIView):
             serializer.is_valid(raise_exception=True)
             serializer.save()
             return Response({"detail": "Submission successful"})
-	
-        
-@login_required
-def update_review(request, pk):
-    review = get_object_or_404(Review, pk=pk)
-    if review.user != request.user:
-        return redirect('home') 
 
-    if request.method == 'POST':
-        form = ReviewForm(request.POST, instance=review)
-        if form.is_valid():
-            form.save()
-            return redirect('review_detail', pk=pk)
-    else:
-        form = ReviewForm(instance=review)
+class UpdateReview(generics.UpdateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer 
+    lookup_field = 'pk'
 
-    return render(request, 'update_review.html', {'form': form})
+    def response(self, serializer):
+        if serializer.instance.user == self.request.user:
+            return Response({"detail": "Submission successful"})
 
+class DeleteReview(generics.DestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    lookup_field = 'pk'
 
-@login_required
-def delete_review(request, pk):
-    review = get_object_or_404(Review, pk=pk)
-    if review.user != request.user:
-        return redirect('home') 
-
-    if request.method == 'POST':
-        review.delete()
-        return redirect('home')  
-
-    return render(request, 'delete_review.html', {'review': review})
-
-
-
-class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 10
-    page_size_query_param = 'page_size'
-    max_page_size = 1000
-
-
-class ReviewView(APIView):
-    pagination_class = StandardResultsSetPagination
-
-
-    def get(self, movie_id):
-        reviews = Review.objects.filter(movie_id=movie_id)
-        serializer = ReviewSerializer(reviews, many=True)
-        paginator = self.pagination_class()
-        return paginator.get_paginated_response(serializer.data)
+class ListReview(generics.ListAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
 
     
-def search_reviews(title=None, rating=None):
-    movies = Review.objects.all()
-
-    q = Q()
-
-    if title:
-        q &= Q(Movie_title__icontains=title)
-
-    if rating:
-        q &= Q(Rating=rating)
-
-    movies = movies.filter(q)
-
-    return movies 
 
